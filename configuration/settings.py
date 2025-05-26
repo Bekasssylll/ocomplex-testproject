1aefe18eb5af8775ad4ad9af0ba835df12b353d9
Bekasssylll <zhenisbekasyl@gmail.com>
2025-05-26 22:42:30 +0500

    +Final logic,tests,some error fixing

--- configuration/settings.py
+++ configuration/settings.py
@@ -52,20 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "configuration.urls"
 
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ]
+
 
 
 TEMPLATES = [
