3b870fbcdea4e7c0740d31c20fe72f95813f602b
Bekasssylll <zhenisbekasyl@gmail.com>
2025-05-26 13:01:46 +0500

    +main logic

--- app/views.py
+++ app/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+headers = {'User-Agent': 'DjangoWeatherApp/1.0'}
+
+
+def test(request):
+    city = request.GET.get('city')
+    context = {}
+    if city:
+        try:
+            lan, lon = define_latitude_longitude(city)
+            data = define_weather(lan, lon)
+
+            context = {
+                'city': city,
+                'current': data['current'],
+                'temperature_unit': data['current_units']['temperature_2m'],
+                'wind_unit': data['current_units']['wind_speed_10m'],
+                'hourly': zip(
+                    data['hourly']['time'][:12],  # первые 12 часов (напр., на график или таблицу)
+                    data['hourly']['temperature_2m'][:12],
+                    data['hourly']['relative_humidity_2m'][:12],
+                    data['hourly']['wind_speed_10m'][:12]
+                )
+            }
+        except Exception as e:
+            context['error'] = f"Ошибка при получении данных: {e}"
+
+    return render(request, 'weather.html', context)
+
+
+def define_latitude_longitude(city):
+    req = requests.get(f'https://nominatim.openstreetmap.org/search?city={city}&format=json', headers=headers)
+    data = req.json()
+    defined = data[0]
+    return defined['lat'], defined['lon']
+
+
+def define_weather(lan, lon):
+    req = requests.get(
+        f'https://api.open-meteo.com/v1/forecast?latitude={lan}&longitude={lon}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m')
+    return req.json()
--- configuration/settings.py
+++ configuration/settings.py
@@ -53,16 +53,33 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "configuration.urls"
 
+# TEMPLATES = [
+#     {
+#         "BACKEND": "django.template.backends.django.DjangoTemplates",
+#         "DIRS": [],
+#         "APP_DIRS": True,
+#         "OPTIONS": {
+#             "context_processors": [
+#                 "django.template.context_processors.request",
+#                 "django.contrib.auth.context_processors.auth",
+#                 "django.contrib.messages.context_processors.messages",
+#             ],
+#         },
+#     },
+# ]
+
+
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],  # сюда можно добавить путь к папке с шаблонами, если надо
+        'APP_DIRS': True,  # обязательно включить, чтобы искать шаблоны внутри приложений
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
